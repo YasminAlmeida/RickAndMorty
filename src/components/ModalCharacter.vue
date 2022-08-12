8730f37c844adb8a815fc633e48d03af166018f2
yasmin <yasmin.almeidavd@gmail.com>
2022-08-12 19:32:04 -0400

    feat("Filter By Category"): "New Functions For The Filter"

--- src/components/ModalCharacter.vue
+++ src/components/ModalCharacter.vue
@@ -1,63 +1,63 @@
 <template class="teste">
   <section class="Containermodal">
     <div class="modal" @click="handleCloseModalOutside">
-      <div class="InsideModal">   
+      <div class="InsideModal">
         <h2 @click="handleCloseModal" class="close">x</h2>
         <div>
-          <img :src="character.image" alt="">
-          <h4>The character is: {{character.name}}</h4>
-          <p>He/She/It is: {{character.status}}</p>
-          <p>Species: {{character.species}}</p>
-          <p>The Location are: {{character.location?.name}}</p>
-        </div>        
+          <img :src="character.image" alt="" />
+          <h4>The character is: {{ character.name }}</h4>
+          <p>He/She/It is: {{ character.status }}</p>
+          <p>Species: {{ character.species }}</p>
+          <p>The Location are: {{ character.location?.name }}</p>
+        </div>
       </div>
     </div>
   </section>
 </template>
 <script>
-import {axiosInstance} from '@/Services.js';
+import { axiosInstance } from '@/Services.js';
 export default {
-  name: 'ModalCharacter', 
-  props:['id','handleCloseModal','handleCloseModalOutside'],
-  data(){
-    return{
-      character:{},
-    }
+  name: 'ModalCharacter',
+  props: ['id', 'handleCloseModal', 'handleCloseModalOutside'],
+  data() {
+    return {
+      character: {},
+    };
   },
-  methods:{
-    fetchData(id){
-       axiosInstance.get(`/character/${id}`).then(r=>{
-      this.character=r.data})
+  methods: {
+    fetchData(id) {
+      axiosInstance.get(`/character/${id}`).then((r) => {
+        this.character = r.data;
+      });
     },
   },
-    watch:{
-    id:{
-      handler(newId){
-        this.fetchData(newId)
-      }
-    }
+  watch: {
+    id: {
+      handler(newId) {
+        this.fetchData(newId);
+      },
+    },
   },
-    created(){
-      this.fetchData(this.id)
-    }
-  }
-
+  created() {
+    this.fetchData(this.id);
+  },
+};
 </script>
 
 <style scoped>
-.modal{
+.modal {
   position: fixed;
   width: 100vw;
   height: 100vh;
   top: 0;
   left: 0;
-  background: rgba(0,0,0,.4);
+  background: rgba(0, 0, 0, 0.4);
   display: flex;
   z-index: 1;
   padding: 10px;
   color: #000;
 }
-.InsideModal{
+.InsideModal {
   position: relative;
   background: #f1faee;
   padding: 50px;
@@ -65,7 +65,7 @@ export default {
   margin: auto;
   max-width: 400px;
 }
-.close{
+.close {
   position: absolute;
   right: 0;
   top: 0;
